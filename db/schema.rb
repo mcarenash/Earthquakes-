9306a4f6371ba3dd490a858f2eea8e7f2470a31c
mcarenash <mariacamilaarenasd@gmail.com>
2015-03-03 10:14:17 -0800

    Removed blank space index

--- db/schema.rb
+++ db/schema.rb
@@ -36,24 +36,6 @@ ActiveRecord::Schema.define(version: 20150217220037) do
   add_index "favorites", ["earthquake_id"], name: "index_favorites_on_earthquake_id"
   add_index "favorites", ["user_id"], name: "index_favorites_on_user_id"
 
-  create_table "installs", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "installs", ["email"], name: "index_installs_on_email", unique: true
-  add_index "installs", ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
